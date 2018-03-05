37a96fc179becc57bf870271fef391387a441a0d
Thomas Sanchez <tomsanchez2017@gmail.com>
2018-03-04 21:13:12 -0600

    Changed the home screen
    So now instead of the two send buttons it is a single add button in the bottom right hand corner. Currently a email icon havent changed it but after pressing it will take you to a screen which would resemble the activity management mode. The TextView string is updated to the input of the two text fields concatinated together when the create button is pressed.  This is a pure visual representation of string maniplation which is how we will interact with the database. Currently still no connections set up properly still in dev phase.

--- app/src/main/java/beaux/thomas/base/ActivityManagement.java
+++ app/src/main/java/beaux/thomas/base/ActivityManagement.java
@@ -1,12 +1,15 @@
 package beaux.thomas.base;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Arrays;
 
 public class ActivityManagement extends AppCompatActivity {
-
-
-
+    public static final String EXTRA_MESSAGE = "beaux.thomas.base.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,18 @@ public class ActivityManagement extends AppCompatActivity {
 
     }
 
+    public void GatherStatInfo(View view){
+
+        EditText StatName = (EditText) findViewById(R.id.StatName);
+        EditText StatType = (EditText) findViewById(R.id.StatType);
+        String SN = StatName.getText().toString();
+        String ST = StatType.getText().toString();
+
+
+        // Capture the layout's TextView and set the string as its text
+        final TextView textView = findViewById(R.id.textView2);
+        textView.setText(SN+ST);
+
+    }
 
 }
--- app/src/main/java/beaux/thomas/base/HomeScreen.java
+++ app/src/main/java/beaux/thomas/base/HomeScreen.java
@@ -1,16 +1,21 @@
 package beaux.thomas.base;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class HomeScreen extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "beaux.thomas.base.MESSAGE";
+    public DatabaseHelper StatboX_Database = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,9 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.homescreen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
+
 
     /** Called when the user taps the Send button */
     public void InputStatMode(View view) {
@@ -28,6 +35,7 @@ public class HomeScreen extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void ActivityManagementMode(View view) {
+
         Intent intent = new Intent(this, ActivityManagement.class);
         startActivity(intent);
     }
