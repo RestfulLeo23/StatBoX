07db79afbcf042a1cec85153e5f145fe72853106
Thomas Sanchez <tomsanchez2017@gmail.com>
2018-04-02 18:49:25 -0500

    Added New Functionality
    Added Stat MetaData

--- app/src/main/java/beaux/thomas/base/ActivityManagement.java
+++ app/src/main/java/beaux/thomas/base/ActivityManagement.java
@@ -3,19 +3,131 @@ package beaux.thomas.base;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ActivityManagement extends AppCompatActivity {
     //public String[] NEW_ACTIVITY = new String[3];
-
+    public int istimer;
+    public int isGPS;
+    int statCount = 0;
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("ON CREATE : ACTIVITY MANAGMENT");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creation);
+        setContentView(R.layout.activity_creation_view);
+
+        System.out.println("FIND TOOLBAR : ACTIVITY MANAGMENT");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        System.out.println("SET SUPPORT TOOL BAR : ACTIVITY MANAGMENT");
+        setSupportActionBar(toolbar);
+
+        System.out.println("AFTER TOOL BAR : ACTIVITY MANAGMENT");
+
+        Switch switch1 = (Switch)findViewById(R.id.timer);
+        Switch switch2 = (Switch)findViewById(R.id.GPS);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                   istimer=1;
+                    System.out.println("^^%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#################### Flip SWITP IS TRUE istimes = 1");
+                    System.out.println(istimer);
+
+                }else{
+                    istimer=0;
+                    System.out.println("^^%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#################### Flip SWITP IS flase istimer = 0");
+                    System.out.println(istimer);
+                }
+            }
+        });
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                    isGPS=1;
+                    System.out.println("^^%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#################### Flip SWITP IS TRUE istimes = 1");
+                    System.out.println(isGPS);
+
+                }else{
+                    isGPS=0;
+                    System.out.println("^^%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#################### Flip SWITP IS flase istimer = 0");
+                    System.out.println(isGPS);
+                }
+            }
+        });
+        addMorestat(switch1);
     }
 
-    public void GatherStatInfo(View view){
+    public void addMorestat(View view){
+
+        if (statCount<5) {
+            statCount += 1;
+            EditText StatName1 = (EditText) findViewById(R.id.StatName);
+            EditText StatName2 = (EditText) findViewById(R.id.StatName2);
+            EditText StatName3 = (EditText) findViewById(R.id.StatName3);
+            EditText StatName4 = (EditText) findViewById(R.id.StatName4);
+            EditText StatName5 = (EditText) findViewById(R.id.StatName5);
+
+            EditText StatType1 = (EditText) findViewById(R.id.StatType);
+            EditText StatType2 = (EditText) findViewById(R.id.StatType2);
+            EditText StatType3 = (EditText) findViewById(R.id.StatType3);
+            EditText StatType4 = (EditText) findViewById(R.id.StatType4);
+            EditText StatType5 = (EditText) findViewById(R.id.StatType5);
+
+            EditText[] names = new EditText[5];
+
+            names[0] = StatName1;
+            names[1] = StatName2;
+            names[2] = StatName3;
+            names[3] = StatName4;
+            names[4] = StatName5;
+
+            EditText[] types = new EditText[5];
+
+            types[0] = StatType1;
+            types[1] = StatType2;
+            types[2] = StatType3;
+            types[3] = StatType4;
+            types[4] = StatType5;
+
+            if (statCount == 0) {
+                StatName1.setVisibility(View.GONE);
+                for (int i = 0; i < 5; i++) {
+                    names[i].setVisibility(View.GONE);
+                }
+            } else {
+                for (int i = 0; i < statCount; i++) {
+                    types[i].setVisibility(View.VISIBLE);
+                    names[i].setVisibility(View.VISIBLE);
+                }
+                for (int j = statCount; j < 5; j++) {
+                    types[j].setVisibility(View.GONE);
+                    names[j].setVisibility(View.GONE);
+                }
+            }
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Max Number of Stat Types",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void GatherStatInfo(View view) {
+
         System.out.println("ACTIVITY CREATION");
         EditText ActivityName = (EditText) findViewById(R.id.ActivityName);
 
@@ -33,37 +145,33 @@ public class ActivityManagement extends AppCompatActivity {
 
         String check = ActivityName.getText().toString();
         String[] New_Act = new String[3];
-/**
-        if(DatabaseHelper.getsInstance(getApplicationContext()).tablesInfo.containsKey(check) == true){
+
+        if (DatabaseHelper.getsInstance(getApplicationContext()).tablesInfo.containsKey(check) != true) {
+            New_Act[0] = ActivityName.getText().toString() + ";"
+                    + StatName1.getText().toString() + ";"
+                    + StatName2.getText().toString() + ";"
+                    + StatName3.getText().toString() + ";"
+                    + StatName4.getText().toString() + ";"
+                    + StatName5.getText().toString();
+
+            New_Act[1] = StatType1.getText().toString() + ";"
+                    + StatType2.getText().toString() + ";"
+                    + StatType3.getText().toString() + ";"
+                    + StatType4.getText().toString() + ";"
+                    + StatType5.getText().toString();
+            New_Act[2] = Integer.toString(istimer) + ";"
+                    + Integer.toString(isGPS);
             Intent resultIntent = new Intent(this, HomeScreen.class);
-            System.out.print("**********Already in DB************");
+
             // TODO Add extras or a data URI to this intent as appropriate.
-            New_Act[0] = "PRINTING ERROR INTENT CODE ";
             resultIntent.putExtra("ACT", New_Act);
-            setResult(666, resultIntent);
+
+            setResult(1, resultIntent);
             finish();
         }
-**/
-        New_Act[0] = ActivityName.getText().toString()+ ";"
-                +StatName1.getText().toString()+ ";"
-                +StatName2.getText().toString()+ ";"
-                +StatName3.getText().toString()+ ";"
-                +StatName4.getText().toString()+ ";"
-                +StatName5.getText().toString();
-        New_Act[1] = StatType1.getText().toString()+ ";"
-                +StatType2.getText().toString()+ ";"
-                +StatType3.getText().toString()+ ";"
-                +StatType4.getText().toString()+ ";"
-                +StatType5.getText().toString();
-
-        Intent resultIntent = new Intent(this, HomeScreen.class);
-
-        // TODO Add extras or a data URI to this intent as appropriate.
-        resultIntent.putExtra("ACT",New_Act);
-
-        setResult(1, resultIntent);
-        finish();
-
+        else {
+            Toast.makeText(getApplicationContext(),"Stat Name Already in Database",Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
--- app/src/main/java/beaux/thomas/base/ViewActivity.java
+++ app/src/main/java/beaux/thomas/base/ViewActivity.java
@@ -1,17 +1,28 @@
 package beaux.thomas.base;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
+import java.util.Set;
 
 public class ViewActivity extends AppCompatActivity {
     public static final int GET_NEW_STAT = 2;
     public static final int OK = 1;
+    final Context context = this;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,6 @@ public class ViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Get the Intent that started this activity and extract the string
@@ -29,6 +39,8 @@ public class ViewActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.StatName);
         textView.setText(message);
+
+        updateStatScreen();
 
     }
 
@@ -55,6 +67,44 @@ public class ViewActivity extends AppCompatActivity {
             }
         }
     }
+    public void updateStatScreen() {
 
+        TextView textView = findViewById(R.id.StatName);
+
+        List<String> Pull = DatabaseHelper.getsInstance(getApplicationContext()).tablesInfo.get(textView.getText().toString());
+        int n = Pull.size();
+        String arr[] = new String[n];
+        arr = Pull.toArray(arr);
+        int size = arr.length;
+
+        TextView[] Stats = new TextView[5];
+        TextView stat1 = findViewById(R.id.Stat1);
+        TextView stat2 = findViewById(R.id.Stat2);
+        TextView stat3 = findViewById(R.id.Stat3);
+        TextView stat4 = findViewById(R.id.Stat4);
+        TextView stat5 = findViewById(R.id.Stat5);
+        Stats[0] = stat1;
+        Stats[1] = stat2;
+        Stats[2] = stat3;
+        Stats[3] = stat4;
+        Stats[4] = stat5;
+
+        if (size == 0) {
+            for (int i = 0; i<5 ; i++){
+                Stats[i].setVisibility(View.GONE);
+            }
+        }
+        else{
+            for (int i = 0; i<size ; i++){
+                Stats[i].setVisibility(View.VISIBLE);
+                Stats[i].setText(arr[i]);
+            }
+            for (int j =size; j < 5 ; j++){
+                Stats[j].setVisibility(View.GONE);
+            }
+        }
+
+
+    }
 
 }
