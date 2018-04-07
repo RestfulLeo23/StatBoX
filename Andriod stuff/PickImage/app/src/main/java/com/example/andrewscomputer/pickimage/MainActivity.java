64afbd47c8381c8645dc982b7133dd7ed9214e50
Andrew <imandrew64@gmail.com>
2018-04-07 17:27:24 -0500

    Branch

---
+++ Andriod stuff/PickImage/app/src/main/java/com/example/andrewscomputer/pickimage/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.andrewscomputer.pickimage;
+
+import java.io.FileNotFoundException;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView textTargetUri;
+    ImageView targetImage;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonLoadImage = (Button)findViewById(R.id.loadimage);
+        textTargetUri = (TextView)findViewById(R.id.targeturi);
+        targetImage = (ImageView)findViewById(R.id.targetimage);
+
+        buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }});
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
