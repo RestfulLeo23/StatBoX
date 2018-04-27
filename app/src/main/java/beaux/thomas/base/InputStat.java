6e4e91242f4027e06716442dc5b1299a35134d2c
Thomas Sanchez <tomsanchez2017@gmail.com>
2018-04-27 17:46:07 -0500

    Adding timer functionality
    DEV

--- app/src/main/java/beaux/thomas/base/InputStat.java
+++ app/src/main/java/beaux/thomas/base/InputStat.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ import static beaux.thomas.base.HomeScreen.EXTRA_MESSAGE;
 public class InputStat extends AppCompatActivity {
 
     public String StatNAME;
+    public static final int GET_TIMER = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +53,7 @@ public class InputStat extends AppCompatActivity {
 
      public void UpdateInputStat(){
         List<String> Pull = DatabaseHelper.getsInstance(getApplicationContext()).tablesInfo.get(StatNAME);
+        String[] t = new String[4];
         int n = Pull.size();
         String arr[] = new String[n];
         arr = Pull.toArray(arr);
@@ -65,11 +68,13 @@ public class InputStat extends AppCompatActivity {
             System.out.println("****THIS IS THE SIZE OF THE TABLES INFO PULL AFTER toARRAY this is reallt a good line so greattttttttttttt:  "+size+"   ****");
 
         }
+
         TextView Stat1 = (TextView) findViewById(R.id.StatName1);
         TextView Stat2 = (TextView) findViewById(R.id.StatName2);
         TextView Stat3 = (TextView) findViewById(R.id.StatName3);
         TextView Stat4 = (TextView) findViewById(R.id.StatName4);
         TextView Stat5 = (TextView) findViewById(R.id.StatName5);
+        TextView Stat6 = (TextView) findViewById(R.id.StatName6);
         TextView[] Stats = new TextView[5];
         Stats[0]= Stat1;
         Stats[1]= Stat2;
@@ -81,12 +86,23 @@ public class InputStat extends AppCompatActivity {
         EditText Statv3 = (EditText) findViewById(R.id.StatVal3);
         EditText Statv4 = (EditText) findViewById(R.id.StatVal4);
         EditText Statv5 = (EditText) findViewById(R.id.StatVal5);
+        EditText Statv6 = (EditText) findViewById(R.id.StatVal6);
         EditText[] Inputs = new EditText[5];
         Inputs[0]= Statv1;
         Inputs[1]= Statv2;
         Inputs[2]= Statv3;
         Inputs[3]= Statv4;
         Inputs[4]= Statv5;
+
+         Button button = findViewById(R.id.timer);
+         t = DatabaseHelper.getsInstance(getApplicationContext()).pullStatTypeMetadata(StatNAME, arr[0]).toArray(t);
+
+
+         if (t[1].equals("0")){
+             button.setVisibility(View.GONE);
+
+
+         }
 
         if (size == 0) {
             for (int i = 0; i<5 ; i++){
@@ -106,8 +122,23 @@ public class InputStat extends AppCompatActivity {
             }
         }
     }
+
     public void openTimer(View view){
         Intent intent = new Intent(this, Timer.class);
-        startActivity(intent);
+        startActivityForResult(intent, GET_TIMER);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case (GET_TIMER): {
+                if (resultCode == 1) {
+                    //String timer
+                    break;
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/beaux/thomas/base/Timer.java
+++ app/src/main/java/beaux/thomas/base/Timer.java
@@ -70,7 +70,7 @@ public class Timer extends AppCompatActivity {
                 container.addView(addView);
 
 
-
+                finish();
             }
         });
 
