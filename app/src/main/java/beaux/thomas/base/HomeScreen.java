af46c11db0992182ee181c2e5a65900bbf27cdbd
Thomas Sanchez <tomsanchez2017@gmail.com>
2018-05-03 18:25:38 -0500

    fixed crash upon delete db

--- app/src/main/java/beaux/thomas/base/HomeScreen.java
+++ app/src/main/java/beaux/thomas/base/HomeScreen.java
@@ -29,7 +29,6 @@ public class HomeScreen extends AppCompatActivity {
     //public DatabaseHelper StatboX_Database;
     public static final int GET_NEW_ACT = 2;
     public static final int OK = 1;
-;
     public boolean clicked = false;
 
     private static final int DRIVE_IMPORT_FINISH = 1;
@@ -153,63 +152,54 @@ public class HomeScreen extends AppCompatActivity {
     }
 
     public void updateHomeScreen() {
-
         ImageButton icon1 = findViewById(R.id.icon1);
         ImageButton icon2 = findViewById(R.id.icon2);
         ImageButton icon3 = findViewById(R.id.icon3);
         ImageButton icon4 = findViewById(R.id.icon4);
         ImageButton icon5 = findViewById(R.id.icon5);
         ImageButton icon6 = findViewById(R.id.icon6);
-
         TextView name1 = findViewById(R.id.STATNAME1);
         TextView name2 = findViewById(R.id.STATNAME2);
         TextView name3 = findViewById(R.id.STATNAME3);
         TextView name4 = findViewById(R.id.STATNAME4);
         TextView name5 = findViewById(R.id.STATNAME5);
         TextView name6 = findViewById(R.id.STATNAME6);
-
         TextView EntryNum1 = findViewById(R.id.entryNumber1);
         TextView EntryNum2 = findViewById(R.id.entryNumber2);
         TextView EntryNum3 = findViewById(R.id.entryNumber3);
         TextView EntryNum4 = findViewById(R.id.entryNumber4);
         TextView EntryNum5 = findViewById(R.id.entryNumber5);
         TextView EntryNum6 = findViewById(R.id.entryNumber6);
-
         TextView statsNum1 = findViewById(R.id.statsNumber1);
         TextView statsNum2 = findViewById(R.id.statsNumber2);
         TextView statsNum3 = findViewById(R.id.statsNumber3);
         TextView statsNum4 = findViewById(R.id.statsNumber4);
         TextView statsNum5 = findViewById(R.id.statsNumber5);
         TextView statsNum6 = findViewById(R.id.statsNumber6);
-
         TextView date1 = findViewById(R.id.recentDate1);
         TextView date2 = findViewById(R.id.recentDate2);
         TextView date3 = findViewById(R.id.recentDate3);
         TextView date4 = findViewById(R.id.recentDate4);
         TextView date5 = findViewById(R.id.recentDate5);
         TextView date6 = findViewById(R.id.recentDate6);
-
         TableRow row1 = findViewById(R.id.Table1);
         TableRow row2 = findViewById(R.id.Table2);
         TableRow row3 = findViewById(R.id.Table3);
         TableRow row4 = findViewById(R.id.Table4);
         TableRow row5 = findViewById(R.id.Table5);
         TableRow row6 = findViewById(R.id.Table6);
-
         TextView timer1 = findViewById(R.id.timer1);
         TextView timer2 = findViewById(R.id.timer2);
         TextView timer3 = findViewById(R.id.timer3);
         TextView timer4 = findViewById(R.id.timer4);
         TextView timer5 = findViewById(R.id.timer5);
         TextView timer6 = findViewById(R.id.timer6);
-
         TextView gps1 = findViewById(R.id.gps1);
         TextView gps2 = findViewById(R.id.gps2);
         TextView gps3 = findViewById(R.id.gps3);
         TextView gps4 = findViewById(R.id.gps4);
         TextView gps5 = findViewById(R.id.gps5);
         TextView gps6 = findViewById(R.id.gps6);
-
         TableRow[] rows = new TableRow[6];
         ImageButton[] IMAGES = new ImageButton[6];
         TextView[] names = new TextView[6];
@@ -225,49 +215,42 @@ public class HomeScreen extends AppCompatActivity {
         IMAGES[3] = icon4;
         IMAGES[4] = icon5;
         IMAGES[5] = icon6;
-
         names[0]=name1;
         names[1]=name2;
         names[2]=name3;
         names[3]=name4;
         names[4]=name5;
         names[5]=name6;
-
         rows[0] = row1;
         rows[1] = row2;
         rows[2] = row3;
         rows[3] = row4;
         rows[4] = row5;
         rows[5] = row6;
-
         dates[0] = date1;
         dates[1] = date2;
         dates[2] = date3;
         dates[3] = date4;
         dates[4] = date5;
         dates[5] = date6;
-
         entries[0] = EntryNum1;
         entries[1] = EntryNum2;
         entries[2] = EntryNum3;
         entries[3] = EntryNum4;
         entries[4] = EntryNum5;
         entries[5] = EntryNum6;
-
         stats[0] = statsNum1;
         stats[1] = statsNum2;
         stats[2] = statsNum3;
         stats[3] = statsNum4;
         stats[4] = statsNum5;
         stats[5] = statsNum6;
-
         gps[0] = gps1;
         gps[1] = gps2;
         gps[2] = gps3;
         gps[3] = gps4;
         gps[4] = gps5;
         gps[5] = gps6;
-
         timer[0] = timer1;
         timer[1] = timer2;
         timer[2] = timer3;
@@ -351,9 +334,7 @@ public class HomeScreen extends AppCompatActivity {
 
 
     public void deleteSettingDB(){
-        System.out.println("DELETING DB");
         DatabaseHelper.getsInstance(getApplicationContext()).death(this);
-        updateHomeScreen();
         android.os.Process.killProcess(android.os.Process.myPid());
         finish();
     }
@@ -376,6 +357,7 @@ public class HomeScreen extends AppCompatActivity {
                 return true;
             case R.id.import_act:
                 GoogleDriveAPIImport();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
